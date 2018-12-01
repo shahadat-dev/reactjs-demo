42def3f18db322307edb75e5ae52dd1d9ea899e8
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-01 17:28:10 +0600

    Updates

---
+++ precache-manifest.2a8f4ab6f3f8f20c458ee752f7fc02de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactjs-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "f8a7f447d01b668367e0",
+    "url": "/reactjs-demo/static/css/main.835be3f3.chunk.css"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactjs-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "0eea2560b40fbe9d52a6",
+    "url": "/reactjs-demo/static/js/1.0eea2560.chunk.js"
+  },
+  {
+    "revision": "2d54b8ec6bb799e8348c",
+    "url": "/reactjs-demo/static/js/runtime~main.2d54b8ec.js"
+  },
+  {
+    "revision": "4e242a313254cdc66f286f6465d7e3f1",
+    "url": "/reactjs-demo/static/media/Spinner.4e242a31.gif"
+  },
+  {
+    "revision": "f8a7f447d01b668367e0",
+    "url": "/reactjs-demo/static/js/main.f8a7f447.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactjs-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactjs-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactjs-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0eea2560b40fbe9d52a6",
+    "url": "/reactjs-demo/static/css/1.d9a2591b.chunk.css"
+  },
+  {
+    "revision": "1b77673e20ce7619ce65948dbd552f18",
+    "url": "/reactjs-demo/index.html"
+  }
+];
